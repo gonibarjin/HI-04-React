3b814259c92115509f88b5010a35cc1637844146
gonibarjin <179527504+gonibarjin@users.noreply.github.com>
2025-01-08 16:36:16 +0100

    Completed assignment 5

--- 5/script.js
+++ 5/script.js
@@ -1,18 +1,21 @@
 // Base URL
-const apiUrl = "https://api.example.com/data";
+const apiUrl = "https://api.sampleapis.com/coffee/hot";
 
 // Function to make a GET request using
 // TODO: 1. find the error and fix it, 2. finish the function
 function fetchData(url) {
-  fetch(url)
+  fetch(apiUrl)
     .then((response) => {
-      if (response.ok) {
+      if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
       }
       return response.json();
     })
     .then((data) => {
       console.log(data);
+    })
+    .catch((error) => {
+      console.error("The fetch did not work as intended", error);
     });
 }
 
@@ -20,11 +23,25 @@ fetchData(apiUrl);
 
 // Function to make a POST request
 // TODO: Implement the function
-function postData(url, data) {}
+function postData(url, data) {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error", error));
+}
 
 const dataToPost = {
-  name: "John Doe",
-  age: 21,
+  title: "Coffee Name", // Expected field
+  description: "A great coffee", // Expected field
+  ingredients: ["coffee", "milk", "sugar"], // Expected field (array)
+  image: "coffee_image_url", // Expected field (string)
+  id: 123, // Expected field (number)
 };
 
 postData(apiUrl, dataToPost);
